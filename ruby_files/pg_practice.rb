1561205ed71c1e0fa61bb72b4edf31673b12acf4
Melissa Patterson <melissa.patterson.va@gmail.com>
2014-12-01 19:21:17 -0600

    add view and delete records

--- ruby_files/pg_practice.rb
+++ ruby_files/pg_practice.rb
@@ -1,15 +1,49 @@
 require 'pg'
 
-def connect 
-  PG::Connection.open(dbname:'classmates_db')
-end
 
-def result(table_name)
-  connect.exec(table_name)
+class Practice
+  attr_reader :database
+  
+  def self.intialize 
+    @database = PG::Connection.open(dbname:'classmates_db')
+  end
+  
+  def create_table
+    sql = %q[
+      CREATE TABLE IF NOT EXISTS people(
+        first_name     VARCHAR,
+        last_name      VARCHAR,
+        twitter        VARCHAR,
+        id             INTEGER SERIAL PRIMARY KEY
+      )  
+    ]
+    database.exec(sql)
+  end
+  
+  def input_data(first, last, twitter)
+    sql = %q[
+      INSERT INTO 
+        people (first_name, last_name, twitter)
+      VALUES 
+        ($1,$2,$3)
+    ]
+    database.exec(sql,[first,last,twitter])
+  end 
+  
+  def view_all_records
+    result = database.exec('SELECT * FROM people')
+    puts result.entries
+  end
+  
+  def delete_a_record(id)
+    sql = %q[
+      DELETE FROM 
+        people
+      WHERE
+         id = ($1)
+    ]
+    database.exec(sql,[id])
+    puts "Entry with id #{id} deleted."
+  end
+  
 end
-
-def input_data(table_name, first_name, last_name, twitter)
-  sql = %q[
-    INSERT INTO
-  ]
-  connect.exec('INSERT INTO ' + table_name+ 
